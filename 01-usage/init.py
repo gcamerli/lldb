5f568f3559832d7fc832f334bad78f28f4b9c27f
Gius. Camerlingo <gcamerli@protonmail.com>
2018-06-21 20:58:35 +0200

    Add 01-usage

---
+++ 01-usage/init.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import lldb
+import commands
+import optparse
+import shlex
+import time
+
+# init function to load the commands
+def init(debugger, command, result, internal_dict):
+        target = debugger.GetSelectedTarget()
+    
+        debugger.SetAsync(True)
+        debugger.HandleCommand("br s -n main")
+        time.sleep(0.2)
+        debugger.HandleCommand("run")
+        time.sleep(0.2)
+
+# Add the init command to the lldb python interpreter
+def __lldb_init_module(debugger, internal_dict):
+        debugger.HandleCommand('command script add -f init.init init')
+        print 'The "init" python command has been installed and is ready for use.'
